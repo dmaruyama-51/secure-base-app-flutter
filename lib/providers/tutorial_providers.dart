bde8769aaf9d16494c96fb243ef8ed0ea166542b
dmaruyama-51 <0501awasuram@gmail.com>
2025-06-13 10:39:08 +0900

    providersディレクトリを削除

--- lib/providers/tutorial_providers.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../repositories/tutorial_repository.dart';
-
-final tutorialRepositoryProvider = Provider<TutorialRepository>((ref) {
-  return TutorialRepository();
-});
