207cc68d62f4642cc33e3c212d14fb2371b4191d
dmaruyama-51 <0501awasuram@gmail.com>
2025-06-19 00:19:22 +0900

    fix cron schedule name

--- database/pg_cron/daily_reflection_batch.sql
+++ database/pg_cron/daily_reflection_batch.sql
@@ -1,7 +1,7 @@
 -- create extension if not exists pg_cron with schema extensions;
 
 select cron.schedule(
-  'run_daily_reflection_batch',
+  'daily_reflection_batch',
   '5 15 * * *', -- JST 0時5分に実行
   $$ select public.run_daily_reflection_batch(); $$
 );
